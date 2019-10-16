9d87d11c45b590a7f447ad482767a90c6059bbd7
Mitchell Rhoads <mitchelldrhoads@gmail.com>
2019-10-15 18:45:55 -0700

    kind of added hard mode

--- imageSpinner.js
+++ imageSpinner.js
@@ -1,27 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function($){
+    var imageBox = $('.slider ul'),
+ imageWidth = $('.slider ul li').first().children('img').width(),
+imageQuantity = $('.slider ul').children('li').length,
+ currentImage = 1;
 
-    $('.container').children('img').each(function(i) { 
-        $(this).fadeOut();
-    }); 
+imageBox.css('width', imageWidth*imageQuantity)
+console.log(imageWidth);
 
-});
+   
+    $('.nav button').on('click', function(){ 
+    var whichButton = $(this).data('nav'); 
+      if(whichButton === 'next'){
+          if(currentImage === imageQuantity){
+              currentImage = 1
+          } else {
+          currentImage++;
+          transition(currentImage, imageWidth);
+          }  
+      } else if(whichButton === 'prev'){
+          if(currentImage === 1){
+              currentImage = imageQuantity;
+              transition(currentImage, imageWidth);
+          } else {
+          currentImage--;
+          transition(currentImage, imageWidth);
+          }
+        }
+    });
+    function transition(currentImageInput, ImageWidthInput){
+        var pxValue = -(currentImageInput-1) * ImageWidthInput
 
-
-
-// $(document).ready(function() {
-//     var _intervalId;
-
-//     function fadeInLastImg()
-//     {
-//         var backImg = $('.container img:first');
-//         backImg.hide();
-//         $('.container' ).append( backImg );
-//         backImg.fadeIn()
-//     };
-
-//     _intervalId = setInterval( function() 
-//     {
-//         fadeInLastImg();
-//     }, 1000 );
-
-// });​
+        imageBox.animate({
+            'left': pxValue
+        });
+    }
+ });
